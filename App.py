d5f8d9c85fc1e1c06747bb731b17b240287fc6eb
root <root@Phoebe.localdomain>
2023-08-01 21:19:15 -0400

    FINALLY

--- App.py
+++ App.py
@@ -17,3 +17,61 @@ debug = DebugToolbarExtension(app)
 
 connect_db(app)
 
+@app.route('/')
+def home():
+    return redirect("/users")
+
+@app.route("/users")
+def list_users():
+
+    users = User.query.all()
+    return render_template("list.html", users=users)
+
+@app.route("/users/new", methods=["GET"])
+def new_form():
+    return render_template('newform.html')
+
+@app.route("/users/new", methods=["POST"])
+def form_submit():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    image_url = request.form['image_url']
+    
+    newbie = User(first_name=first_name, last_name=last_name, image_url=image_url)
+    db.session.add(newbie)
+    db.session.commit()
+
+    return redirect("/")
+
+@app.route("/users/<int:user_id>")
+def show_user(user_id):
+    
+    user = User.query.get_or_404(user_id)
+    return render_template("detail.html", user=user)
+
+@app.route("/users/<int:user_id>/edit")
+def edit_user(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template('edit.html', user=user)
+
+@app.route("/users/<int:user_id>/edit", methods=["POST"])
+def save_user(user_id):
+
+    user = User.query.get_or_404(user_id)
+    user.first_name = request.form['first_name']
+    user.last_name = request.form['last_name']
+    user.image_url = request.form['image_url']
+
+    
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect("/users")
+
+@app.route("/users/<int:user_id>/delete", methods=["POST"])
+def delete_user(user_id):
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
+
+    return redirect("/users")
