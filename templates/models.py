9f59eb743a2d0e220b033d5120459e959ee54b39
rrobles1591 <111623195+rrobles1591@users.noreply.github.com>
2023-09-30 14:02:13 -0400

    Add files via upload

---
+++ templates/models.py
@@ -0,0 +1,73 @@
+import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+
+    __tablename__="User"
+
+    def __repr__(self):
+        p= self
+        return f"<User id={p.id} first_name={p.first_name} last_name={p.last_name} image_url={p.image_url}>"
+
+    id = db.Column(db.Integer,
+                   primary_key=True, 
+                   autoincrement=True)
+    first_name = db.Column(db.String(50),
+                           nullable=False)
+    last_name = db.Column(db.String(50),
+                          nullable=(False))
+
+    image_url = db.Column(db.Text)
+
+    posts = db.relationship("Post", backref="user", cascade="all, delete-orphan")
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    #To create database with this model (ipython %run
+    #  app.py, db.create_all() )
+
+class Post (db.Model):
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
+    # @property
+    # def friendly_date(self):
+    #     return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
+
+class PostTag(db.Model): 
+
+    __tablename__="posts_tags"
+
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), primary_key=True)
+
+class Tag(db.Model):
+
+    __tablename_="tags"
+    id = db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.Text, nullable=False, unique=True)
+
+    posts = db.relationship(
+        'Post',
+        secondary="posts_tags",
+        # cascade="all,delete",
+        backref="tags"
+    )
+
+    
